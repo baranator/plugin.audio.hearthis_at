9f0b1786f8551110b79f953f2bfbe3b917f26070
Eike Baran <eikebaran89@gmail.com>
2017-06-26 20:54:14 +0200

    fixing missing functions, cleaning up routing

--- addon.py
+++ addon.py
@@ -33,83 +33,81 @@ def api_call(query):
 @plugin.route('/')
 def main_menu():
     items = [
-                {'label': _("Recently added"), 'path': plugin.url_for('show_feed_first', ftype='new', page=1, first=True)},
-                {'label': _("Popular"), 'path': plugin.url_for('show_feed_first', ftype='popular', page=1, first=True)},
+                {'label': _("Recently added"), 'path': plugin.url_for('show_feed_first', ftype='new')},
+                {'label': _("Popular"), 'path': plugin.url_for('show_feed_first', ftype='popular')},
                 {'label': _("Genres"), 'path': plugin.url_for('show_genres')},
                 {'label': _("Search"), 'path': plugin.url_for('search_first', skey='x', page=1, first=True)}
             ]
 
-    return items
+    return plugin.finish(items)
 
-@plugin.route('/user/<user>/<page>/<first>', name='show_user_first', options={'page': '1', 'first': True})
+
+@plugin.route('/playlist/<plink>')
+def show_playlist(plink):
+    plist = api_call('set/'+plink)
+    return list_tracks(plist)
+
+@plugin.route('/user/<user>/playlists', name='show_users_playlists_first', options={'page': '1', 'first': 'True'})
+@plugin.route('/user/<user>/playlists/<page>')
+def show_users_playlists(user, page, first=False):
+    results = api_call(add_pp('%s/?type=playlists' % (user), page))  
+    items = []
+    for l in results:
+        items.append({'label': l['title'], 'path': plugin.url_for('show_playlist', plink=l['permalink'])})#,'is_playable': True})
+
+    pagination={'call': 'show_users_playlists', 'args':{'user': user, 'page': int(page)}}
+    return list_tracks(results, pagination, first)
+
+
+@plugin.route('/user/<user>/likes', name='show_users_likes_first', options={'page': '1', 'first': 'True'})
+@plugin.route('/user/<user>/likes/<page>')
+def show_users_likes(user, page, first=False):
+    results = api_call(add_pp('%s/?type=likes' % (user), page))    
+    pagination={'call': 'show_users_likes', 'args':{'user': user, 'page': int(page)}}
+    return list_tracks(results, pagination, first)
+
+
+@plugin.route('/user/<user>/<page>/<first>', name='show_user_first', options={'page': '1', 'first': 'True'})
 @plugin.route('/user/<user>/<page>')
 def show_user(user, page, first=False):
     u = api_call(user)
     results = api_call(add_pp('%s/?type=tracks' % (user), page))
     pagination={'call': 'show_user', 'args':{'user': user, 'page': int(page)}}
     selectors = [
-                    {'label': 'Playlists ('+str(u['playlist_count'])+')', 'path': plugin.url_for('show_users_playlists_firstpage', user=user)},
-                    {'label': 'Likes ('+str(u['likes_count'])+')', 'path': plugin.url_for('show_users_likes_firstpage', user=user)}
+                    {'label': '%s (%s)' % (_("Playlists"),str(u['playlist_count'])), 'path': plugin.url_for('show_users_playlists_first', user=user)},
+                    {'label': '%s (%s)' % (_("Likes"), str(u['likes_count'])), 'path': plugin.url_for('show_users_likes_first', user=user)}
                 ]
     return list_tracks(results, pagination, first, pre=selectors)
 
-@plugin.route('/playlist/<plink>')
-def play_playlist(plink):
-    plist = api_call('set/'+plink)
-    return list_tracks(plist)
-
-@plugin.route('/user/<user>/playlists', name='show_users_playlists_firstpage', options={'page': '1'})
-@plugin.route('/user/<user>/playlists/<page>')
-def show_users_playlists(user, page):
-    lists = api_call('%s/?type=playlists?page=%d&count=%d' % (user, int(page), PER_PAGE))
-    items = []
-    for l in lists:
-        items.append({'label': l['title'], 'path': plugin.url_for('play_playlist', plink=l['permalink'])})#,'is_playable': True})
-    return items + nextpage(plugin.url_for('show_users_playlists', user=user, page=int(page)+1), len(items)) 
-
-@plugin.route('/user/<user>/likes', name='show_users_likes_firstpage', options={'page': '1'})
-@plugin.route('/user/<user>/likes/<page>')
-def show_users_likes(user, page):
-    tracks = api_call('%s/?type=likes?page=%d&count=%d' % (user, int(page), PER_PAGE))
-    return list_tracks(tracks) + nextpage(plugin.url_for('show_users_likes', user=user, page=int(page)+1), len(tracks)) 
     
 @plugin.route('/genres')
 def show_genres():
-    genres = api_call('categories')
+    results = api_call('categories')
     items = []
-    for g in genres:
+    for g in results:
         items.append({
             'label': g['name'],
-            'path': plugin.url_for('show_genre_firstpage', genre=g['id'])
+            'path': plugin.url_for('show_genre_first', genre=g['id'])
         })
     
-    return items
-
-def pn_button(pagination, direction):
-    page = pagination['args']['page']
-    if pagination != None and (direction == 1 or page > 1):
-        args = copy.deepcopy(pagination['args'])
-        args['page'] += direction
-        lbl = '%s (%d - %d)' % ((_("next"), page*PER_PAGE+1,(page+1)*PER_PAGE) if direction == 1 else (_("previous"), (page-1)*PER_PAGE+1,page*PER_PAGE))
-        return {'label': lbl, 'path': plugin.url_for(pagination['call'], **args)}
-    else:
-        return None
+    return plugin.finish(items)
 
 
-
-@plugin.route('/feeds/<ftype>/<page>/<first>', name='show_feed_first')
+@plugin.route('/feeds/<ftype>/<page>/<first>', name='show_feed_first', options={'page': '1', 'first': 'True'})
 @plugin.route('/feeds/<ftype>/<page>')
 def show_feed(ftype, page, first=False):
     results = api_call(add_pp('feed/?type=%s' % (ftype), page))
     pagination={'call': 'show_feed', 'args':{'ftype': ftype, 'page': int(page)}}
     return list_tracks(results, pagination, first)
 
-@plugin.route('/genre/<genre>/<page>/<first>', name='show_genre_first')
+
+@plugin.route('/genre/<genre>/<page>/<first>', name='show_genre_first', options={'page': '1', 'first': 'True'})
 @plugin.route('/genre/<genre>/<page>')
 def show_genre(genre, page, first=False):
     results = api_call(add_pp('categories/%s/' % (genre), page, sep='?'))
     pagination={'call': 'show_genre', 'args':{'genre': genre, 'page': int(page)}}
     return list_tracks(results, pagination, first)
+
 
 @plugin.route('/search_for/<stype>/<skey>/<page>/<first>', name='search_for_first')
 @plugin.route('/search_for/<stype>/<skey>/<page>')
@@ -125,8 +123,9 @@ def search_for(stype, skey, page, first=False):
         items += [pn_button(pagination, 1)]
         return items
 
+
 #TODO: sometimes jumps out of plugin while browsing search results
-@plugin.route('/search/<skey>/<page>/<first>', name='search_first')
+@plugin.route('/search/<skey>/<page>/<first>', name='search_first', options={'page': '1', 'first': 'True', 'skey': None})
 @plugin.route('/search/<skey>/<page>')
 def search(skey, page, first=False):
     if first:
@@ -138,9 +137,9 @@ def search(skey, page, first=False):
             return None
     results = api_call(add_pp('search?t=%s/' % (skey), page))
     selectors = [
-                        {'label': _("Search for artist only"), 'path': plugin.url_for('search_for_first', stype='user', skey=skey, page=1, first=True)},
-                        {'label': _("Search for tracks only"), 'path': plugin.url_for('search_for_first', stype='tracks', skey=skey, page=1, first=True)}
-                        ]
+                        {'label': _("Search for artist only"), 'path': plugin.url_for('search_for_first', stype='user', skey=skey, page=1, first='True')},
+                        {'label': _("Search for tracks only"), 'path': plugin.url_for('search_for_first', stype='tracks', skey=skey, page=1, first='True')}
+                ]
     pagination={'call': 'search', 'args':{'skey': skey, 'page': int(page)}}
     return list_tracks(results, pagination, first, pre = selectors)
     
@@ -150,6 +149,7 @@ def play_track(user,trackid):
     playurl='https://hearthis.at/'+user+'/'+trackid+'/listen'
     plugin.log.info('Playing: %s'%playurl)
     return plugin.set_resolved_url(playurl)   
+
 
 def list_tracks(tracklist, pagination = None, first=False, pre=[], post=[]):
     items = pre
@@ -176,12 +176,24 @@ def list_tracks(tracklist, pagination = None, first=False, pre=[], post=[]):
     items.append(pn_button(pagination, 1))
     
     #only skip history if turning pages, not if opening first page initially
-    if pagination != None and not first:
+    if pagination != None and first != 'True':
         ul=True
     else:
         ul=False
     
-    return plugin.finish(items, update_listing=ul)# if pagination != None else False)
+    return plugin.finish(items, update_listing=ul)
+
+
+def pn_button(pagination, direction):
+    page = pagination['args']['page']
+    if pagination != None and (direction == 1 or page > 1):
+        args = copy.deepcopy(pagination['args'])
+        args['page'] += direction
+        lbl = '%s (%d - %d)' % ((_("next"), page*PER_PAGE+1,(page+1)*PER_PAGE) if direction == 1 else (_("previous"), (page-1)*PER_PAGE+1,page*PER_PAGE))
+        return {'label': lbl, 'path': plugin.url_for(pagination['call'], **args)}
+    else:
+        return None
+
 
 def add_pp(call, page, sep = '&'):
     return '%s%spage=%d&count=%d' % (call, sep, int(page), PER_PAGE)
