153a780592d484757d6f34241b5798f84c874b7d
Eike Baran <eikebaran89@gmail.com>
2017-07-03 02:39:51 +0200

    tidying up the code; added some screenshots and other infos to addon.xml

--- addon.py
+++ addon.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from kodiswift import Plugin
-from kodiswift import actions
-from kodiswift import xbmcgui
+from kodiswift import Plugin, actions
+from kodiswift import xbmc, xbmcgui
 import xbmcaddon
 import requests
 import copy
@@ -10,23 +9,16 @@ import os
 
 
 # Global vars
-PLUGIN = Plugin()
-plugin = PLUGIN
+plugin = Plugin()
+addon = plugin.addon
 
-ADDON = PLUGIN.addon
-USER_AGENT = "Kodi ("+platform.system() + ") hearthis.at-Plugin/" + ADDON.getAddonInfo('version')
-plugin.log.info(USER_AGENT)
-PER_PAGE = 15
+USER_AGENT = "Kodi ("+platform.system() + ") hearthis.at-Plugin/" + addon.getAddonInfo('version')
 HEARTHIS = 'hearthis.at'
-API_BASE_URL="https://api-v2.hearthis.at/"
+API_BASE_URL = "https://api-v2.hearthis.at/"
 USER = plugin.get_storage('user_data')
+ADDON_PATH = xbmc.translatePath(addon.getAddonInfo('path')).decode('utf-8')
 
-ADDON_PATH = ADDON.getAddonInfo('path')
-
-def get_image(img):
-    return os.path.join(ADDON_PATH,'resources','images', img)
-
-strings = {
+STRINGS = {
         'genres'        : 30000,
         'playlists'     : 30001,
         'show_artist'   : 30002,
@@ -50,15 +42,17 @@ strings = {
 }
 
 
-win = xbmcgui.Window(xbmcgui.getCurrentWindowId())
+def get_image(img):
+    return os.path.join(ADDON_PATH,'resources','images', img)
 
 
 def _(string):
-    tstring = strings.get(string)
+    tstring = STRINGS.get(string)
     if tstring == None:
         return None
     else:
-        return ADDON.getLocalizedString(strings[string])
+        return addon.getLocalizedString(tstring)
+
 
 def api_call(path, params=None, rtype='GET', data=None, json=True):
     if logged_in():
@@ -68,9 +62,7 @@ def api_call(path, params=None, rtype='GET', data=None, json=True):
         params['secret'] = USER['data']['secret']
     url = API_BASE_URL+path
     headers = {'user-agent': USER_AGENT}
-    
-    plugin.log.info('api-call: %s with data %s' % (url,str(data)))
-     
+    plugin.log.info('api-call: %s with data %s' % (url, str(data)))
     if rtype == 'GET':
         r = requests.get(url, params=params, headers=headers)
     else:
@@ -81,59 +73,6 @@ def api_call(path, params=None, rtype='GET', data=None, json=True):
     else:
         return r.text
     
-
-
-
-@plugin.route('/')
-def main_menu():
-    login()
-    items1 = [
-                {'label': _('recently_added'), 'icon': get_image('new.png'), 'path': plugin.url_for('show_feed_first', ftype='new')},
-                {'label': _('popular'), 'icon': get_image('popular.png'), 'path': plugin.url_for('show_feed_first', ftype='popular')},
-                {'label': _('genres'), 'icon': get_image('genres.png'), 'path': plugin.url_for('show_genres')},
-            ]
-    
-    if logged_in():
-        items_private = [   
-                {'label': _('my_likes'), 'icon': get_image('likes.png'), 'path': plugin.url_for('show_users_likes_first', user=USER['data']['permalink'])},
-                {'label': _('following'), 'icon': get_image('following.png'), 'path': plugin.url_for('show_following_first', user=USER['data']['permalink'])},
-                        ]
-    else:
-        items_private = []
-    items2 = [
-                {'label': _('search'), 'icon': get_image('search.png'),  'path': plugin.url_for('search')}
-            ]
-    
-    
-    
-    return plugin.finish(items1 + items_private + items2)
-
-
-@plugin.route('/playlist/<plink>')
-def show_playlist(plink):
-    plist = api_call('set/'+plink)
-    return list_tracks(plist)
-
-
-@plugin.route('/user/<user>/playlists', name='show_users_playlists_first', options={'page': '1', 'first': 'True'})
-@plugin.route('/user/<user>/playlists/<page>')
-def show_users_playlists(user, page, first=False):
-    results = api_call(user, add_pp({'type': 'playlists'}, page))  
-    items = []
-    for l in results:
-        items.append({'label': l['title'], 'path': plugin.url_for('show_playlist', plink=l['permalink'])})#,'is_playable': True})
-
-    pagination={'call': 'show_users_playlists', 'args':{'user': user, 'page': int(page)}}
-    return list_tracks(results, pagination, first)
-
-
-@plugin.route('/user/<user>/likes', name='show_users_likes_first', options={'page': '1', 'first': 'True'})
-@plugin.route('/user/<user>/likes/<page>')
-def show_users_likes(user, page, first=False):
-    results = api_call(user, add_pp({'type': 'likes'}, page))    
-    pagination={'call': 'show_users_likes', 'args':{'user': user, 'page': int(page)}}
-    return list_tracks(results, pagination, first)
-
 
 def list_users(userlist, pagination = None, first=False, pre=[], post=[]):
     if isinstance(userlist, dict):
@@ -148,134 +87,14 @@ def list_users(userlist, pagination = None, first=False, pre=[], post=[]):
                                             context_item_toggle('follow', u['following'], {'user': u['permalink']})
                                         ],
                       'path': plugin.url_for('show_user_first', user=u['permalink'], page=1, first=True)})
-            
     items = items + post
     items.append(pn_button(pagination, 1, len(userlist)))
-    
     #only skip history if turning pages, not if opening first page initially
     if pagination != None and first != 'True':
         ul=True
     else:
         ul=False
     return plugin.finish(items, update_listing=ul)
-
-
-@plugin.route('/user/<user>/following', name='show_following_first', options={'page': '1', 'first': 'True'})
-@plugin.route('/user/<user>/following/<page>')
-def show_following(user, page, first=False):
-    results = api_call(user+'/following', add_pp({}, page))    
-    pagination={'call': 'show_following', 'args':{'user': user, 'page': int(page)}}
-    return list_users(results, pagination, first)
-
-
-@plugin.route('/user/<user>/<page>/<first>', name='show_user_first', options={'page': '1', 'first': 'True'})
-@plugin.route('/user/<user>/<page>')
-def show_user(user, page, first=False):
-    u = api_call(user)
-    results = api_call(user, add_pp({'type': 'tracks'}, page))  
-    pagination={'call': 'show_user', 'args':{'user': user, 'page': int(page)}}
-    selectors = [
-                    {'label': '%s (%s)' % (_('playlists'), str(u['playlist_count'])), 'icon': get_image('playlist.png'), 'path': plugin.url_for('show_users_playlists_first', user=user)},
-                    {'label': '%s (%s)' % (_('likes'), str(u['likes_count'])), 'icon': get_image('likes.png'), 'path': plugin.url_for('show_users_likes_first', user=user)}
-                ]
-    return list_tracks(results, pagination, first, pre=selectors)
-
-    
-@plugin.route('/genres')
-def show_genres():
-    results = api_call('categories')
-    items = []
-    for g in results:
-        items.append({
-            'label': g['name'],
-            'path': plugin.url_for('show_genre_first', genre=g['id'])
-        })
-    
-    return plugin.finish(items)
-
-
-@plugin.route('/feeds/<ftype>/<page>/<first>', name='show_feed_first', options={'page': '1', 'first': 'True'})
-@plugin.route('/feeds/<ftype>/<page>')
-def show_feed(ftype, page, first=False):
-    results = api_call('feed', add_pp({'type': ftype}, page))  
-    pagination={'call': 'show_feed', 'args':{'ftype': ftype, 'page': int(page)}}
-    return list_tracks(results, pagination, first)
-
-
-@plugin.route('/genre/<genre>/<page>/<first>', name='show_genre_first', options={'page': '1', 'first': 'True'})
-@plugin.route('/genre/<genre>/<page>')
-def show_genre(genre, page, first=False):
-    results = api_call('categories/'+genre, add_pp({}, page))  
-    pagination={'call': 'show_genre', 'args':{'genre': genre, 'page': int(page)}}
-    return list_tracks(results, pagination, first)
-
-
-@plugin.route('/search_for/<stype>/<skey>/<page>/<first>', name='search_for_first')
-@plugin.route('/search_for/<stype>/<skey>/<page>')
-def search_for(stype, skey, page, first=False):
-    results = api_call('search', add_pp({'type': stype, 't': skey}, page))  
-    pagination={'call': 'search_for', 'args':{'stype': stype, 'skey': skey, 'page': int(page)}}
-    if stype == 'tracks':
-        return list_tracks(results, pagination, first)
-    elif stype == 'user':
-        return list_users(results, pagination, first)
-
-
-#TODO: sometimes jumps out of plugin while browsing search results
-@plugin.route('/search')
-def search():
-    kb = xbmc.Keyboard ('', _('Search'), False)
-    kb.doModal()
-    if kb.isConfirmed():
-        skey = kb.getText()
-    else:
-        return None
-    selectors = [
-                        {'label': _('search_artist'), 'icon': get_image('search_artist.png'), 'path': plugin.url_for('search_for_first', stype='user', skey=skey, page=1, first='True')},
-                        {'label': _('search_track'), 'icon': get_image('search_track.png'), 'path': plugin.url_for('search_for_first', stype='tracks', skey=skey, page=1, first='True')}
-                ]
-    return plugin.finish(selectors)
-    
-   
-@plugin.route('/play/<user>/<trackid>')
-def play_track(user, trackid):
-    playurl='https://hearthis.at/'+user+'/'+trackid+'/listen'
-    plugin.log.info('Playing: %s'%playurl)
-    return plugin.set_resolved_url(playurl)   
-
-
-@plugin.route('/logged_in/toggle/<prop>')
-def toggle_prop(prop):
-    parms = plugin.request.args
-    PLUGIN.log.info(str(parms))
-    if prop == 'like':
-        results = api_call(parms['user'][0]+'/'+parms['trackid'][0]+'/like')
-        r = results['liked']
-    elif prop == 'follow':
-        results = api_call(parms['user'][0]+'/follow')
-        r = results['follow']
-    elif prop == 'reshare':
-        results = api_call(parms['user'][0]+'/'+parms['trackid'][0]+'/reshare')
-    PLUGIN.log.info(str(results))
-    PLUGIN.notify('set' if r else 'reset')
-    return None
-
-def context_item_toggle(prop, state, parms):
-    if logged_in():
-        if state:
-            lbl = 'rm_'+prop
-        else:
-            lbl = 'add_'+prop
-        parms['prop'] = prop
-        ar_follow = ( _(lbl), actions.update_view(plugin.url_for('toggle_prop', **parms)))
-    else:
-        ar_follow = None
-    return ar_follow
-
-
-def show_user_context_item(user):
-    show_user_url = plugin.url_for('show_user_first', user=user, page=1, first='True')
-    return ( _('show_artist'), actions.update_view(show_user_url))
 
 
 def list_tracks(tracklist, pagination = None, first=False, pre=[], post=[]):
@@ -308,7 +127,6 @@ def list_tracks(tracklist, pagination = None, first=False, pre=[], post=[]):
         })
     items = items + post
     items.append(pn_button(pagination, 1, len(tracklist)))
-    
     #only skip history if turning pages, not if opening first page initially
     if pagination != None and first != 'True':
         ul=True
@@ -317,7 +135,7 @@ def list_tracks(tracklist, pagination = None, first=False, pre=[], post=[]):
     return plugin.finish(items, update_listing=ul)
 
 
-def pn_button(pagination, direction, length=PER_PAGE):
+def pn_button(pagination, direction, length):
     page = pagination['args']['page']
     if pagination != None :
         args = copy.deepcopy(pagination['args'])
@@ -327,7 +145,7 @@ def pn_button(pagination, direction, length=PER_PAGE):
                 return None
             lbl=_("previous")
         else:
-            if length < PER_PAGE:
+            if length < int(addon.getSetting('page_count')):
                 return None
             lbl=_("next")
         return {'label': '[%s ...]' % (lbl), 'path': plugin.url_for(pagination['call'], **args)}
@@ -337,19 +155,19 @@ def pn_button(pagination, direction, length=PER_PAGE):
 
 def add_pp(obj, page):
     obj['page'] = int(page)
-    obj['count'] = PER_PAGE
+    obj['count'] = int(addon.getSetting('page_count'))
     return obj
 
 
 def login():
-    if ADDON.getSetting('login_enabled') == 'false':
+    if addon.getSetting('login_enabled') == 'false':
         if logged_in():
             result = api_call('logout/')
             USER['data'] = None
             plugin.log.info("Logging out: "+str(result))
         return
-    password = ADDON.getSetting('password')
-    email = ADDON.getSetting('email')
+    password = addon.getSetting('password')
+    email = addon.getSetting('email')
     if logged_in():
         return
     result = api_call('login/', rtype='POST', data={'email': email, 'password': password})
@@ -363,6 +181,168 @@ def login():
 
 def logged_in():
     return USER['data'] != None
+
+
+def context_item_toggle(prop, state, parms):
+    if logged_in():
+        if state:
+            lbl = 'rm_'+prop
+        else:
+            lbl = 'add_'+prop
+        parms['prop'] = prop
+        ar_follow = ( _(lbl), actions.update_view(plugin.url_for('toggle_prop', **parms)))
+    else:
+        ar_follow = None
+    return ar_follow
+
+
+def show_user_context_item(user):
+    show_user_url = plugin.url_for('show_user_first', user=user, page=1, first='True')
+    return ( _('show_artist'), actions.update_view(show_user_url))
+
+
+@plugin.route('/')
+def main_menu():
+    login()
+    items1 = [
+                {'label': _('recently_added'), 'icon': get_image('new.png'), 'path': plugin.url_for('show_feed_first', ftype='new')},
+                {'label': _('popular'), 'icon': get_image('popular.png'), 'path': plugin.url_for('show_feed_first', ftype='popular')},
+                {'label': _('genres'), 'icon': get_image('genres.png'), 'path': plugin.url_for('show_genres')},
+            ]
+    if logged_in():
+        items_private = [   
+                {'label': _('my_likes'), 'icon': get_image('likes.png'), 'path': plugin.url_for('show_users_likes_first', user=USER['data']['permalink'])},
+                {'label': _('following'), 'icon': get_image('following.png'), 'path': plugin.url_for('show_following_first', user=USER['data']['permalink'])},
+                        ]
+    else:
+        items_private = []
+    items2 = [
+                {'label': _('search'), 'icon': get_image('search.png'),  'path': plugin.url_for('search')}
+            ]
+    return plugin.finish(items1 + items_private + items2)
+
+
+@plugin.route('/playlist/<plink>')
+def show_playlist(plink):
+    plist = api_call('set/'+plink)
+    return list_tracks(plist)
+
+
+@plugin.route('/user/<user>/playlists', name='show_users_playlists_first', options={'page': '1', 'first': 'True'})
+@plugin.route('/user/<user>/playlists/<page>')
+def show_users_playlists(user, page, first=False):
+    results = api_call(user, add_pp({'type': 'playlists'}, page))  
+    items = []
+    for l in results:
+        items.append({'label': l['title'], 'path': plugin.url_for('show_playlist', plink=l['permalink'])})
+    pagination={'call': 'show_users_playlists', 'args':{'user': user, 'page': int(page)}}
+    return list_tracks(results, pagination, first)
+
+
+@plugin.route('/user/<user>/likes', name='show_users_likes_first', options={'page': '1', 'first': 'True'})
+@plugin.route('/user/<user>/likes/<page>')
+def show_users_likes(user, page, first=False):
+    results = api_call(user, add_pp({'type': 'likes'}, page))    
+    pagination={'call': 'show_users_likes', 'args':{'user': user, 'page': int(page)}}
+    return list_tracks(results, pagination, first)
+
+
+@plugin.route('/user/<user>/following', name='show_following_first', options={'page': '1', 'first': 'True'})
+@plugin.route('/user/<user>/following/<page>')
+def show_following(user, page, first=False):
+    results = api_call(user+'/following', add_pp({}, page))    
+    pagination={'call': 'show_following', 'args':{'user': user, 'page': int(page)}}
+    return list_users(results, pagination, first)
+
+
+@plugin.route('/user/<user>/<page>/<first>', name='show_user_first', options={'page': '1', 'first': 'True'})
+@plugin.route('/user/<user>/<page>')
+def show_user(user, page, first=False):
+    u = api_call(user)
+    results = api_call(user, add_pp({'type': 'tracks'}, page))  
+    pagination={'call': 'show_user', 'args':{'user': user, 'page': int(page)}}
+    selectors = [
+                    {'label': '%s (%s)' % (_('playlists'), str(u['playlist_count'])), 'icon': get_image('playlist.png'), 'path': plugin.url_for('show_users_playlists_first', user=user)},
+                    {'label': '%s (%s)' % (_('likes'), str(u['likes_count'])), 'icon': get_image('likes.png'), 'path': plugin.url_for('show_users_likes_first', user=user)}
+                ]
+    return list_tracks(results, pagination, first, pre=selectors)
+
+    
+@plugin.route('/genres')
+def show_genres():
+    results = api_call('categories')
+    items = []
+    for g in results:
+        items.append({
+            'label': g['name'],
+            'path': plugin.url_for('show_genre_first', genre=g['id'])
+        })
+    return plugin.finish(items)
+
+
+@plugin.route('/feeds/<ftype>/<page>/<first>', name='show_feed_first', options={'page': '1', 'first': 'True'})
+@plugin.route('/feeds/<ftype>/<page>')
+def show_feed(ftype, page, first=False):
+    results = api_call('feed', add_pp({'type': ftype}, page))  
+    pagination={'call': 'show_feed', 'args':{'ftype': ftype, 'page': int(page)}}
+    return list_tracks(results, pagination, first)
+
+
+@plugin.route('/genre/<genre>/<page>/<first>', name='show_genre_first', options={'page': '1', 'first': 'True'})
+@plugin.route('/genre/<genre>/<page>')
+def show_genre(genre, page, first=False):
+    results = api_call('categories/'+genre, add_pp({}, page))  
+    pagination={'call': 'show_genre', 'args':{'genre': genre, 'page': int(page)}}
+    return list_tracks(results, pagination, first)
+
+
+@plugin.route('/search_for/<stype>/<skey>/<page>/<first>', name='search_for_first')
+@plugin.route('/search_for/<stype>/<skey>/<page>')
+def search_for(stype, skey, page, first=False):
+    results = api_call('search', add_pp({'type': stype, 't': skey}, page))  
+    pagination={'call': 'search_for', 'args':{'stype': stype, 'skey': skey, 'page': int(page)}}
+    if stype == 'tracks':
+        return list_tracks(results, pagination, first)
+    elif stype == 'user':
+        return list_users(results, pagination, first)
+
+
+@plugin.route('/search')
+def search():
+    kb = xbmc.Keyboard ('', _('Search'), False)
+    kb.doModal()
+    if kb.isConfirmed():
+        skey = kb.getText()
+    else:
+        return None
+    selectors = [
+                        {'label': _('search_artist'), 'icon': get_image('search_artist.png'), 'path': plugin.url_for('search_for_first', stype='user', skey=skey, page=1, first='True')},
+                        {'label': _('search_track'), 'icon': get_image('search_track.png'), 'path': plugin.url_for('search_for_first', stype='tracks', skey=skey, page=1, first='True')}
+                ]
+    return plugin.finish(selectors)
+    
+   
+@plugin.route('/play/<user>/<trackid>')
+def play_track(user, trackid):
+    playurl='https://hearthis.at/'+user+'/'+trackid+'/listen'
+    plugin.log.info('Playing: %s' % playurl)
+    return plugin.set_resolved_url(playurl)   
+
+
+@plugin.route('/logged_in/toggle/<prop>')
+def toggle_prop(prop):
+    parms = plugin.request.args
+    if prop == 'like':
+        results = api_call(parms['user'][0]+'/'+parms['trackid'][0]+'/like')
+        r = results['liked']
+    elif prop == 'follow':
+        results = api_call(parms['user'][0]+'/follow')
+        r = results['follow']
+    elif prop == 'reshare':
+        results = api_call(parms['user'][0]+'/'+parms['trackid'][0]+'/reshare')
+    plugin.log.info(str(results))
+    #plugin.notify('set' if r else 'reset')
+    return None
 
 
 if __name__ == '__main__':
